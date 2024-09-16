4b0d0fafaa7af3fa1fd1249eb2f3ecd2f996c3b5
Neg00se <evg.matyushko@gmail.com>
2024-09-16 21:40:46 +0300

    Configure services

--- ContactManager/Program.cs
+++ ContactManager/Program.cs
@@ -1,3 +1,10 @@
+using Data;
+using Data.Interfaces;
+using Data.Repositories;
+using Microsoft.EntityFrameworkCore;
+using Services.Interfaces;
+using Services.Services;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +13,13 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.AddDbContext<ContactMangerDbContext>(options =>
+options.UseSqlServer(builder.Configuration.GetConnectionString("Default")));
+
+builder.Services.AddScoped<IContactRepository, ContactRepository>();
+builder.Services.AddScoped<IContactService, ContactService>();
+
 
 var app = builder.Build();
 
